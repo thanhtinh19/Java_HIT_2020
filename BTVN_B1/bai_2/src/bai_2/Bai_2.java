669cbf44f62d0a79f9b62660c0cce9d5a82cf28f
thanhtinh19 <thanhtinh2k1@gmail.com>
2020-09-26 09:00:55 +0700

    BTVN

---
+++ BTVN_B1/bai_2/src/bai_2/Bai_2.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai_2;
+
+/**
+ *
+ * @author NGO THI THANH TINH
+ */
+public class Bai_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println("*       *     *     * * * *   * * * *   *     *");
+        System.out.println("|       |    / \\    |      *  |      *   \\   /");
+        System.out.println("* ----- *   *---*   * * * *   * * * *      *");
+        System.out.println("|       |  /     \\  |         |            |");
+        System.out.println("*       * *       * *         *            *");
+        System.out.println();
+        System.out.println();
+        System.out.println("*    *  ******  *       *       *");
+        System.out.println("| \\  |  |        \\     / \\     /");
+        System.out.println("*  * *  ******    *   *   *   *");
+        System.out.println("|   \\|  |          \\ /     \\ /");
+        System.out.println("*    *  ******      *       *");
+        System.out.println();
+        System.out.println();
+        System.out.println("*   *  ******      *      * * * * ");
+        System.out.println(" \\ /   |          / \\     |       *");
+        System.out.println("  *    ******    *---*    * * * * ");
+        System.out.println("  |    |        /     \\   |    \\");
+        System.out.println("  *    ******  *       *  *     *");
+    }
+    
+}
